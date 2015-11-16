4e2a92dd6fd33906f398481884594d2029ce43dd
Ryan Pavlik <ryan@sensics.com>
2015-11-16 17:40:26 -0600

    Extend the list of device descriptors

--- lib/osvr_compatibility_aggregator/device_descriptors.rb
+++ lib/osvr_compatibility_aggregator/device_descriptors.rb
@@ -3,9 +3,39 @@ require 'osvr_compatibility_aggregator/source_collection'
 module OsvrCompatibilityAggregator
   def self.device_descriptors
     SourceCollection.new do |s|
-      s.github_file repo: 'OSVR/OSVR-Core', file: 'plugins/multiserver/com_osvr_Multiserver_OSVRHackerDevKit.json'
-      s.github_file repo: 'OSVR/OSVR-Core', file: 'plugins/multiserver/com_osvr_Multiserver_RazerHydra.json'
-      s.github_file repo: 'OSVR/OSVR-Core', file: 'plugins/multiserver/com_osvr_Multiserver_YEI_3Space_Sensor.json'
+      s.collection autodetected: true do |s|
+        s.github_file repo: 'OSVR/OSVR-Core',
+                      file: 'plugins/multiserver/com_osvr_Multiserver_OSVRHackerDevKit.json'
+
+        s.github_file repo: 'OSVR/OSVR-Core',
+                      file: 'plugins/multiserver/com_osvr_Multiserver_RazerHydra.json'
+
+      end
+      s.github_file repo: 'OSVR/OSVR-Core',
+                    file: 'plugins/multiserver/com_osvr_Multiserver_YEI_3Space_Sensor.json',
+                    sample_config: 'https://github.com/OSVR/OSVR-Core/blob/master/apps/sample-configs/osvr_server_config.HDKv1_YEI.xrightydown.sample.json'
+
+      s.github_file repo: 'OSVR/OSVR-Core',
+                    file: 'plugins/opencv/com_osvr_VideoCapture_OpenCV.json',
+                    sample_config: 'https://github.com/OSVR/OSVR-Core/blob/master/apps/sample-configs/osvr_server_config.OpenCVCamera.sample.json'
+
+      s.github_file repo: 'OSVR/OSVR-Leap-Motion',
+                    file: 'com_osvr_LeapMotion.json'
+
+      s.github_file repo: 'OSVR/OSVR-SMI',
+                    file: 'com_osvr_SMI_EyeTracker.json',
+                    sample_config: 'osvr_server_config.SMIeyeTracker.sample.json',
+                    download: '[Pre-built 32-bit Windows binaries](http://access.osvr.com/binary/smi)'
+
+      s.github_file repo: 'OSVR/OSVR-Oculus-Rift',
+                    file: 'com_osvr_OculusRift.json',
+                    sample_config: 'osvr_server_config.oculusrift.sample.json',
+                    download: '[Pre-built Windows binaries](http://access.osvr.com/binary/oculus)'
+
+      # No device descriptor?
+      #s.github_file repo: 'OSVR/OSVR-Nod',
+      #              file: 'plugins/opencv/com_osvr_VideoCapture_OpenCV.json',
+      #              sample_config: 'https://github.com/OSVR/OSVR-Core/blob/master/apps/sample-configs/osvr_server_config.OpenCVCamera.sample.json'
     end
   end
 end
