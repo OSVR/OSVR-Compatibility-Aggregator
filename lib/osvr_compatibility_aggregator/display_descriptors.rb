ccbdff3fe68f2367c2478ca5fb292841c612a122
Ryan Pavlik <ryan@sensics.com>
2015-11-17 12:42:38 -0600

    Use github_dir to get all the display descriptors shipped

--- lib/osvr_compatibility_aggregator/display_descriptors.rb
+++ lib/osvr_compatibility_aggregator/display_descriptors.rb
@@ -3,7 +3,8 @@ require 'osvr_compatibility_aggregator/source_collection'
 module OsvrCompatibilityAggregator
   def self.display_descriptors
     SourceCollection.new do |s|
-      s.github_file repo: 'OSVR/OSVR-Core', file: 'apps/displays/OSVR_HDK_1_1.json'
+      s.github_dir repo: 'OSVR/OSVR-Core',
+                   dir: 'apps/displays'
     end
   end
 end
