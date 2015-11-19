2999dc7e25faf3bc81c6bfe4d4fa67d8930d7617
Ryan Pavlik <ryan@sensics.com>
2015-11-18 18:34:11 -0600

    More spec improvements.

--- spec/parse_spec.rb
+++ spec/parse_spec.rb
@@ -5,24 +5,24 @@ describe OsvrCompatibilityAggregator, '#parse' do
     expect(OsvrCompatibilityAggregator.parse(data: %q(
     {
       "basicJson": true
-    })))
+    }))).to be_truthy
   end
   it 'parses some simple JSON from a string' do
     expect(OsvrCompatibilityAggregator.parse(%q(
     {
       "basicJson": true
-    })))
+    }))).to be_truthy
   end
   it 'parses some JSON with C-style comments from a string' do
     expect(OsvrCompatibilityAggregator.parse(%q(
     {
       "commentsCStyle": true /* block comment*/
-    })))
+    }))).to be_truthy
   end
   it 'parses some JSON with C++-style comments from a string' do
     expect(OsvrCompatibilityAggregator.parse(%q(
     {
       "commentsCxxStyle": true // single-line comment
-    })))
+    }))).to be_truthy
   end
 end
